b47db38b5f51c0f8c34b6c3e6d691c33417d54ec
Rajiya Sultana <rssumu04@gmail.com>
2025-01-06 11:41:08 +0600

    Destroy object
    New Update:
    Destroy objects
    Else-if statements

--- Clash/Assets/Scripts/DistroyOutofBound.cs
+++ Clash/Assets/Scripts/DistroyOutofBound.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class DistroyOutofBound : MonoBehaviour
 {
     public float upBound = 2;
+    public float downBound = -10;
     void Start()
     {
         
@@ -11,6 +12,10 @@ public class DistroyOutofBound : MonoBehaviour
     void Update()
     {
         if(transform.position.z > upBound)
+        {
+            Destroy(gameObject);
+        }
+        else if(transform.position.z < downBound)
         {
             Destroy(gameObject);
         }
