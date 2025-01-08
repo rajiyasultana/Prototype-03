c8e5789ed99a4588e7fa897d97c6db2c9b84c423
Rajiya Sultana <rssumu04@gmail.com>
2025-01-09 00:16:16 +0600

    SpwanManager added

---
+++ Clash/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] enemyPrefebs;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
