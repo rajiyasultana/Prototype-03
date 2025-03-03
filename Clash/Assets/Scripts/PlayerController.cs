0abe3853258806ca2f9d26b5f02c0c188d3f55f6
Rajiya Sultana <rssumu04@gmail.com>
2025-03-03 12:38:30 +0600

    Setup virtual camera.

--- Clash/Assets/Scripts/PlayerController.cs
+++ Clash/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,12 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using Vector2 = System.Numerics.Vector2;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody m_rigidbody;
     private PlayerInputActions m_playerInputActions;
     private readonly float m_jumpForce = 5f;
-    private readonly float m_moveSpeed = 5f;
+    private readonly float m_moveSpeed = 10f;
 
     private void Awake()
     {
@@ -37,37 +36,9 @@ public class PlayerController : MonoBehaviour
 
     private void Move(InputAction.CallbackContext context)
     {
-        Vector3 input = context.ReadValue<Vector3>(); // ✅ Correctly reading Vector3
+        Vector3 input = context.ReadValue<Vector3>(); 
 
         m_rigidbody.velocity = new Vector3(input.x * m_moveSpeed, m_rigidbody.velocity.y, input.z * m_moveSpeed);
     }
-    /*public float horizontalInput;
-    public float speed = 8;
-    public float xRange = 12;
-
-    public GameObject projectilePrefeb;
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        if(transform.position.x < -xRange)
-        {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-        }
-        if (transform.position.x > xRange)
-        {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-        }
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput *  Time.deltaTime * speed);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            //Lunch a projectile from the player.
-            Instantiate(projectilePrefeb, transform.position, projectilePrefeb.transform.rotation);
-        }
-    }*/
+    
 }
