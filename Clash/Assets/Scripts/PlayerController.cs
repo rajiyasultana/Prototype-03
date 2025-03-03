14295fe5fccf2aada3574e86bae624b367078cee
Rajiya Sultana <rssumu04@gmail.com>
2025-03-03 12:02:12 +0600

    set new input system to player.

--- Clash/Assets/Scripts/PlayerController.cs
+++ Clash/Assets/Scripts/PlayerController.cs
@@ -1,21 +1,45 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Vector2 = System.Numerics.Vector2;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody m_rigidbody;
+    private PlayerInputActions m_playerInputActions;
+    private readonly float m_jumpForce = 5f;
+    private readonly float m_moveSpeed = 5f;
 
     private void Awake()
     {
         m_rigidbody = GetComponent<Rigidbody>();
+        
+        m_playerInputActions = new PlayerInputActions();
+        m_playerInputActions.Player.Enable();
+        
+        m_playerInputActions.Player.Jump.performed += Jump;
+        m_playerInputActions.Player.Move.performed += Move;
     }
-    public void Jump(InputAction.CallbackContext context)
+
+    private void OnDestroy()
+    {
+        m_playerInputActions.Player.Jump.performed -= Jump;
+        m_playerInputActions.Player.Move.performed -= Move;
+    }
+
+    private void Jump(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            m_rigidbody.AddForce(Vector3.up * 5f, ForceMode.Impulse); 
+            m_rigidbody.AddForce(Vector3.up * m_jumpForce, ForceMode.Impulse); 
         }
         
+    }
+
+    private void Move(InputAction.CallbackContext context)
+    {
+        Vector3 input = context.ReadValue<Vector3>(); // ✅ Correctly reading Vector3
+
+        m_rigidbody.velocity = new Vector3(input.x * m_moveSpeed, m_rigidbody.velocity.y, input.z * m_moveSpeed);
     }
     /*public float horizontalInput;
     public float speed = 8;
