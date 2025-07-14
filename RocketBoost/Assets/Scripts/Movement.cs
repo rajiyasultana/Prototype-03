1248014415dfa2fda6185070a75475767293141a
Rajiya Sultana <rssumu04@gmail.com>
2025-07-14 17:29:07 +0600

    Added rotation to the player

--- RocketBoost/Assets/Scripts/Movement.cs
+++ RocketBoost/Assets/Scripts/Movement.cs
@@ -4,7 +4,9 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction move;
+    [SerializeField] InputAction rotation;
     [SerializeField] float jumpForce = 100f;
+    [SerializeField] float rotationForce = 100f;
 
     Rigidbody rb;
 
@@ -16,13 +18,39 @@ public class Movement : MonoBehaviour
     private void OnEnable()
     {
         move.Enable();
+        rotation.Enable();
     }
 
     private void FixedUpdate()
+    {
+        ProcessMove();
+        ProcessRotation();
+
+    }
+
+    private void ProcessMove()
     {
         if (move.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * jumpForce * Time.fixedDeltaTime);
         }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationValue = rotation.ReadValue<float>();
+        if(rotationValue < 0)
+        {
+            ApplyRotation(rotationForce);
+        }
+        else if (rotationValue > 0)
+        {
+            ApplyRotation(-rotationForce);
+        }
+    }
+
+    private void ApplyRotation(float rotateThisFram)
+    {
+        transform.Rotate(Vector3.forward * rotateThisFram * Time.fixedDeltaTime);
     }
 }
