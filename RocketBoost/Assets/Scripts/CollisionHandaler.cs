b7a82db3482d9a9f785d86b50af9fe41b34abd13
Rajiya Sultana <rssumu04@gmail.com>
2025-07-15 22:26:24 +0600

    Update CollisionHandaler.cs

--- RocketBoost/Assets/Scripts/CollisionHandaler.cs
+++ RocketBoost/Assets/Scripts/CollisionHandaler.cs
@@ -7,7 +7,7 @@ public class CollisionHandaler : MonoBehaviour
     {
         switch (other.gameObject.tag)
         {
-            case "Friendly" :
+            case "Friendly":
                 Debug.Log("Everything is fine!");
                 break;
 
@@ -16,26 +16,32 @@ public class CollisionHandaler : MonoBehaviour
                 break;
 
             default:
-                ReloadLevel();
+                StartCrushSequence();
                 break;
         }
+    }
 
-        void LoadNextScene()
+    void LoadNextScene()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = currentScene + 1;
+        if(nextScene == SceneManager.sceneCountInBuildSettings)
         {
-            int currentScene = SceneManager.GetActiveScene().buildIndex;
-            int nextScene = currentScene + 1;
-            if(nextScene == SceneManager.sceneCountInBuildSettings)
-            {
-                nextScene = 0;
-            }
-
-            SceneManager.LoadScene(nextScene);
+            nextScene = 0;
         }
 
-        void ReloadLevel()
-        {
-            int currentScene = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(currentScene);
-        }
+        SceneManager.LoadScene(nextScene);
+    }
+
+    void StartCrushSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", 2f);
+    }
+
+    void ReloadLevel()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
     }
 }
